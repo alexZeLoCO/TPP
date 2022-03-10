b96a52a432cc236524b79bc071a6c9479ee72c77
Alex <alejandro.rod.lop@gmail.com>
2022-03-10 17:44:40 +0100

    Added cRecursividad

---
+++ Warcraft/Warcraft01/src/wc1/base/FAbstracta.java
@@ -0,0 +1,9 @@
+package wc1.base;
+public interface FAbstracta {
+
+    public Unidad creaInfanteria ();
+    public Unidad creaArquero();
+    public Unidad creaJinete();
+    public Unidad creaMA();
+    
+}
