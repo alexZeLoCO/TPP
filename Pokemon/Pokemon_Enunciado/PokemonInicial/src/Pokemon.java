e41edb64abcfff049c0d92a3a4f6cc0cb6778903
alexZeLoCO <alejandro.rod.lop@gmail.com>
2022-03-20 12:14:07 +0100

    Added pokemon default files

---
+++ Pokemon/Pokemon_Enunciado/PokemonInicial/src/Pokemon.java
@@ -0,0 +1,12 @@
+
+public abstract class Pokemon {
+	public void golpea(){
+		// golpea a algún pokemon enemigo a tiro
+	}
+	public void retirate(){
+		// vuelve a su pokeball
+	}
+	public void defender(){
+		// el pokemon se concentra para recibir menos daño en el siguiente ataque
+	}
+}
